ac8010339ce0196c83915928edc89d48f06f481f
Sergey Mihaylishin <smihaylishin@sugarcrm.com>
2022-01-26 16:17:35 +0300

    CSC-3032 Django 3 support

--- invitations/migrations/0003_auto_20151126_1523.py
+++ invitations/migrations/0003_auto_20151126_1523.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import django.db.models.deletion
-from django.conf import settings
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
