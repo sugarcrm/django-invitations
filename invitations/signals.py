e8de8bb7d5b57efdffc07430b0f64f82d934c922
Sergey Mihaylishin <smihaylishin@sugarcrm.com>
2022-02-01 16:15:25 +0300

    CSC-3049 Upgrade Python to version 3.9

--- invitations/signals.py
+++ invitations/signals.py
@@ -1,13 +1,14 @@
 from django.dispatch import Signal
 
-invite_url_sent = Signal(providing_args=['invite_url_sent', 'inviter'])
-invite_accepted = Signal(providing_args=['email'])
-
+invite_url_sent = Signal()
 """
 @receiver(invite_url_sent, sender=Invitation)
 def invite_url_sent(sender, instance, invite_url_sent, inviter, **kwargs):
     pass
+"""
 
+invite_accepted = Signal()
+"""
 @receiver(invite_accepted, sender=auth.models.AnonymousUser)
 def invite_accepted(sender, request, email, **kwargs):
     pass
